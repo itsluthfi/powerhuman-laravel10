cc4ed38662b5c4a11ef30e455c2a1291b9bdff41
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-04-13 19:30:53 +0700

    feat(controllers): create user controller and add api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\API\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/company', [CompanyController::class, 'fetch']);
+
+Route::post('/login', [UserController::class, 'login']);
