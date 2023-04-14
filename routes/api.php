f3f4f58571103fa366dd1eec0758449fd861cf38
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-04-14 15:36:24 +0700

    refactor(routes): grouping api routes by controller

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,19 @@ use App\Http\Controllers\API\CompanyController;
 |
 */
 
-Route::get('/company', [CompanyController::class, 'fetch'])->middleware('auth:sanctum');
-Route::post('/company', [CompanyController::class, 'create'])->middleware('auth:sanctum');
+// Auth API
+Route::name('auth.')->group(function () {
+    Route::post('login', [UserController::class, 'login'])->name('login');
+    Route::post('register', [UserController::class, 'register'])->name('register');
 
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [UserController::class, 'fetch'])->middleware('auth:sanctum');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [UserController::class, 'logout'])->name('logout');
+        Route::get('user', [UserController::class, 'fetch'])->name('fetch');
+    });
+});
+
+// Company API
+Route::prefix('company')->middleware('auth:sanctum')->name('company.')->group(function () {
+    Route::get('', [CompanyController::class, 'fetch'])->name('fetch');
+    Route::post('', [CompanyController::class, 'create'])->name('create');
+});
